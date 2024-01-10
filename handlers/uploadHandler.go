06b48fe605cfd879dfa321c1b6b5ff0125e36ecb
shukla rituparn <shukla.r@phystech.edu>
2024-01-10 18:10:42 +0300

    recieving files from the user

--- handlers/uploadHandler.go
+++ handlers/uploadHandler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -12,7 +13,10 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		fmt.Fprintf(w, "You made a get request to the upload Endpoint")
 	}
-	fmt.Println(r)
+	p, _ := io.ReadAll(r.Body)
+	fmt.Printf("%s\n", p)
 }
 
 //So the post request is getting made to the endpoint
+
+//and I am getting the data
