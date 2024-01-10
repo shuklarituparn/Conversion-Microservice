e8f1187aa9931091f27d77a433e78b746db811bf
shukla rituparn <shukla.r@phystech.edu>
2024-01-10 18:03:46 +0300

    added the code

--- handlers/uploadHandler.go
+++ handlers/uploadHandler.go
@@ -6,5 +6,13 @@ import (
 )
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "YES IT HITS")
+	if r.Method == "POST" {
+		fmt.Fprintf(w, "You made a post request to the upload Endpoint")
+	}
+	if r.Method == "GET" {
+		fmt.Fprintf(w, "You made a get request to the upload Endpoint")
+	}
+	fmt.Println(r)
 }
+
+//So the post request is getting made to the endpoint
