8353c7ceadc45ed9f1b54a92ef71e115a0a1c1f0
shukla rituparn <shukla.r@phystech.edu>
2024-01-12 02:12:03 +0300

    upload is working and file is saving correctly

--- handlers/uploadHandler.go
+++ handlers/uploadHandler.go
@@ -49,6 +49,8 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 //TODO: Now need to create a producer for this message. Can make a folder for init kafka and call the functions here
 //TODO: Also left to add the Mongo Upload and good UI
 
+//TODO: Need to check buffalo and if it makes it any better than from scratch
+
 //So the post request is getting made to the endpoint
 
 //and I am getting the data
