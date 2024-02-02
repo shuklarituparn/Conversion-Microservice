786cca51ce3039094b4f2e1790d82371634d1996
shukla rituparn <shukla.r@phystech.edu>
2024-02-03 00:46:50 +0300

    added the code

---
+++ email/email_template.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"github.com/matcornic/hermes/v2"
+
+	"os"
+)
+
+func main() {
+	// Configure hermes by setting a theme and your product info
+	h := hermes.Hermes{
+		// Optional Theme
+		// Theme: new(Default)
+		Product: hermes.Product{
+			// Appears in header & footer of e-mails
+			Name: "Video_conversion_service",
+			Link: "https://knowing-gannet-actively.ngrok-free.app/",
+			// Optional product logo
+			Logo: "https://iili.io/J09GHml.md.jpg",
+		},
+	}
+
+	email := hermes.Email{
+		Body: hermes.Body{
+			Name: "Rituparn Shukla",
+			Intros: []string{
+				"Welcome to Video Conversion Service! We're very excited to have you on board.",
+			},
+			Actions: []hermes.Action{
+				{
+					Instructions: "To get started with Video_Conversion_Service, please click here:",
+					Button: hermes.Button{
+						Color: "#22BC66", // Optional action button color
+						Text:  "Confirm your account",
+						Link:  "https://knowing-gannet-actively.ngrok-free.app/",
+					},
+				},
+			},
+			Outros: []string{
+				"Need help, or have questions? Just reply to this email, we'd love to help.",
+			},
+		},
+	}
+
+	// Generate an HTML email with the provided contents (for modern clients)
+	emailBody, err := h.GenerateHTML(email)
+	if err != nil {
+		panic(err) // Tip: Handle error with something else than a panic ;)
+	}
+
+	// Generate the plaintext version of the e-mail (for clients that do not support xHTML)
+	_, err = h.GeneratePlainText(email)
+	if err != nil {
+		panic(err) // Tip: Handle error with something else than a panic ;)
+	}
+
+	// Optionally, preview the generated HTML e-mail by writing it to a local file
+	err = os.WriteFile("preview.html", []byte(emailBody), 0644)
+	if err != nil {
+		panic(err) // Tip: Handle error with something else than a panic ;)
+	}
+}
+
+//TODO: SO CAN BASICALLY GENERATE DIFFERENT TEMPLATES FOR DIFFERENT USE AND THEN USE KAFKA TO SEND TO USER
